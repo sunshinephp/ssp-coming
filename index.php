bdc1838cb618f5982f148c0d84c75382cfee333a
Adam Culp <adamculp@uws.net>
2013-05-14 12:13:01 -0400

    Added 2014 large logo and main coming soon page.

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <title>SunshinePHP Developer Conference 2014</title>
+
+    <style type="text/css">
+        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+    </style>
+
+    <link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<div style="margin:auto;text-align: center;width: 800px;">
+    <img src="images/sunshinephp_logo_2014_800px.png" alt="SunshinePHP Developer Conference 2014" />
+    
+    <p>Get ready, mark your calendars! If you thought the <a href="http://2013.sunshinephp.com" title="2013 SunshinePHP Developer Conference">2013 SunshinePHP Developer Conference</a> was awesome, you won't want to miss 2014!</p>
+    <p>&nbsp;</p>
+    <p>
+        Add yourself to the SunshinePHP mailing list and stay informed:<br />
+        (Note: Not necessary if you added yourself for 2013)
+    </p>
+    
+    <!-- Begin MailChimp Signup Form -->
+    <div id="mc_embed_signup">
+        <form action="http://uniquewebsites.us6.list-manage.com/subscribe/post?u=f6982e96ad914a99e45e6b5a9&amp;id=48ad0ac116" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <label for="mce-EMAIL">Signup for future notifications</label>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </form>
+    </div>
+    <!--End mc_embed_signup-->
+    
+</div>
+
+</body>
+</html>
