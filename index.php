ad370bcb327cd844a8caaf67e49122b7e14ab7c3
Adam Culp <adamculp@uws.net>
2013-06-08 13:02:20 -0400

    updated coming soon page to be new design, color scheme and layout.

--- index.php
+++ index.php
@@ -1,54 +1,68 @@
+<!doctype html>
 <html>
-<head>
-    <title>SunshinePHP Developer Conference 2014</title>
-
-    <style type="text/css">
-        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-            /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-    </style>
-
-    <link href="http://cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-
-<div style="margin:auto;text-align: center;width: 800px;">
-    <img src="images/sunshinephp_logo_2014_800px.png" alt="SunshinePHP Developer Conference 2014" />
-    
-    <p>Get ready, mark your calendars! If you thought the <a href="http://2013.sunshinephp.com" title="2013 SunshinePHP Developer Conference">2013 SunshinePHP Developer Conference</a> was awesome, you won't want to miss 2014!</p>
-    <p>&nbsp;</p>
-    <p>
-        Add yourself to the SunshinePHP mailing list and stay informed:
-    </p>
-    
-    <!-- Begin MailChimp Signup Form -->
-    <div id="mc_embed_signup">
-        <form action="http://uniquewebsites.us6.list-manage.com/subscribe/post?u=f6982e96ad914a99e45e6b5a9&amp;id=48ad0ac116" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <label for="mce-EMAIL">Signup for future notifications</label>
-            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-        </form>
-    </div>
-    <!--End mc_embed_signup-->
-    
-</div>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-35868187-1']);
-  _gaq.push(['_setDomainName', 'sunshinephp.com']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
-<div>
-    <p>As a rough estimate we expect to announce the Call For Papers, as well as announcing Sponsor packages in August/September 2013.</p>
-</div>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sunshine PHP Developer Conference 2014</title>
+        <link href="css/all.min.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="wrap">
+        	<header class="container-fluid" id="header-home">
+                <div class="row-fluid">
+                    <div class="span12 text-center">
+                        <h1>
+                        	<img src="images/sunshinephp_logo_2014_800px.png" alt="SunshinePHP Developer Conference - Feb. 6-8 2014" id="ssphplogo" class="fade-in one" />
+                        </h1>
+                    </div>
+                </div>
+            </header>
+            <section class="container-fluid" id="hero-home">
+                <article class="row-fluid">
+                    <div class="span12 text-center fade-in two">
+                        <h4>Get ready, mark your calendars!</h4>
+                        <p>If you thought the <a href="http://2013.sunshinephp.com" title="2013 Sunshine PHP Developer Conference" class="whiteLink">2013 Sunshine PHP Developer Conference</a> was awesome, you won't want to miss 2014!</p>
+                        <p>Add yourself to the Sunshine PHP mailing list and stay informed:</p>
+                    </div>
+                </article>
+            </section>
+            <section class="container-fluid" id="form-home">
+                <article class="row-fluid">
+                    <!-- Begin MailChimp Signup Form -->
+                    <div class="span12 text-center" id="mc_embed_signup">
+                        <form action="http://uniquewebsites.us6.list-manage.com/subscribe/post?u=f6982e96ad914a99e45e6b5a9&amp;id=48ad0ac116" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate form-signup-home fade-in three" target="_blank" novalidate>
+                            <label for="mce-EMAIL">Signup for future notifications</label>
+                            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                            <div class="clear text-center clearfix">
+                                <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                            </div>
+                        </form>
+                    </div>
+                    <!--End mc_embed_signup--> 
+                </article>
+            </section>
+        </div>
+        <footer class="container-fluid" id="footer-home">
+            <div class="row-fluid">
+                <h5 class="text-center capitalize fade-in four">Sunshine PHP Conference</h5>
+            </div>
+        </footer>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.9.1.min.js"%3E%3C/script%3E'))</script> 
+        <script src="js/bootstrap.min.js"></script> 
+        <script type="text/javascript">
+        
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-35868187-1']);
+          _gaq.push(['_setDomainName', 'sunshinephp.com']);
+          _gaq.push(['_trackPageview']);
+        
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        
+        </script>
+    </body>
 </html>
